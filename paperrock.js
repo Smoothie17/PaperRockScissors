98cc3fb76caecd51aa21f565af863c6fd19e0aeb
Smoothie17 <jason.cooper17@hotmail.com>
2022-08-04 17:12:27 -0600

    JS start

---
+++ paperrock.js
@@ -0,0 +1,20 @@
+const choices = ['rock', 'paper', 'scissors']
+
+function game () {
+
+}
+
+
+
+function playRound() {
+  
+
+}
+function playerChoice() {
+
+}
+
+function computerChoice() {
+  return choices[Math.floor(Math.random()*choices*3)]
+
+}
