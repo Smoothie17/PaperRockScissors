7c8470988ff9359e6b520cafada8fd16fbc24adb
Smoothie17 <jason.cooper17@hotmail.com>
2022-08-04 18:42:53 -0600

    new update

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const choices = ['rock', 'paper', 'scissors']
+const winners =[];
 
-function game () {
+function game() {
   playRound();
+  for(let i = 0; i <= 5; i++ ){
+    playRound()
+  }
+  logWins;
 
 }
 
@@ -10,37 +15,61 @@ function game () {
 function playRound() {
   const playerSelection = playerChoice();
   const computerSelection = computerChoice();
-
+  console.log(computerSelection)
+  const winner = checkWinner(playerSelection, computerSelection);
+  console.log(winner);
+  winners.push(winner);
 
 }
 function playerChoice() {
-  let input = prompt('Type Rock, Paper, or Scissors');
+  let input = prompt("Type Rock, Paper, or Scissors");
   while (input == null) {
-    input = prompt ('Type Rock, Paper, or Scissors')
+    input = prompt ("Type Rock, Paper, or Scissors");
   }
   input = input.toLowerCase();
-  let check = validateInput(input)
-  while  (check == false){
-    prompt( 'Type Rock, Paper, or Scissors, spelling needs to be exact');
+  let check = validateInput(input);
+  while (check == false) {
+    input = prompt("Type Rock, Paper, or Scissors, spelling needs to be exact");
     while (input == null) {
-      input = prompt ('Type Rock, Paper, or Scissors');
+      input = prompt ("Type Rock, Paper, or Scissors");
     }
+
     input = input.toLowerCase();
     check = validateInput(input);
   }
-  //console.log(input);
+  return input;
   
 
 }
 
 function computerChoice() {
-  return choices[Math.floor(Math.random()*choices*3)]
+  return choices[Math.floor(Math.random()*choices.length)];
 
 }
 function validateInput(choice) {
  return choices.includes(choice);
-   
-  }
+ 
+   }
+
+function checkWinner (choiceP, choiceC) {
+  if (choiceP === choiceC) {
+    return "Tie";
+  } else if (
+    (choiceP === "rock" && choiceC == "scissors") || 
+    (choiceP === "paper" && choiceC == "rock" )  ||
+    (choiceP === "scissors" &&  choiceC == "paper")
+  ) {
+  return "Player";
+} else {
+  return "Computer";
+ }
+}
+
+function logWins (){
+ let playerWins = winners.filter((item) => item == 'Player')
+}
+
+
 
 
 game();
